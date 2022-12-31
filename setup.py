e84c81a3884b1a861d42a19f75d1ccddb2e840ff
Pulin Agrawal <pulinagrawal@users.noreply.github.com>
2022-12-30 19:28:40 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,21 +9,7 @@ setup(name='Questgen',
       url="https://github.com/ramsrigouthamg/Questgen.ai",
       install_requires=[
          
-           'transformers==3.0.2',
-           'sense2vec==2.0.0',
-           'strsim==0.0.3',
-           'six==1.15.0',
-           'networkx==2.6.3',
-           'numpy==1.21.5',
-           'scipy==1.4.1',
-           'scikit-learn==1.0.2',
-           'unidecode==1.3.4',
-           'future==0.16.0',
-           'joblib==1.1.0',
-           'pytz==2018.9',
-           'python-dateutil==2.8.2',
-           'flashtext==2.7',
-           'pandas==1.3.5'
+        
       ],
       package_data={'Questgen': ['questgen.py', 'mcq.py', 'train_gpu.py', 'encoding.py']}
       )
